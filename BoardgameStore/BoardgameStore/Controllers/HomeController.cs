2ddb74c7fa2ac9856420d77f1774bb95c5540f15
shumsss19 <shumsss19@gmail.com>
2018-05-21 02:49:36 +0300

    Improve the appearance of the pages

--- BoardgameStore/BoardgameStore/Controllers/HomeController.cs
+++ BoardgameStore/BoardgameStore/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Net;
 using System.Net.Mime;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using BoardgameStore.Models;
 
 namespace BoardgameStore.Controllers
@@ -16,15 +17,13 @@ namespace BoardgameStore.Controllers
     public class HomeController : Controller
     {
         private BoardgameStoreDBEntities db = new BoardgameStoreDBEntities();
-
-        // GET: Boardgames
+        
         public ActionResult Catalog()
         {
             var boardgame = db.Boardgame.Include(b => b.Author).Include(b => b.Complexity).Include(b => b.Publisher);
             return View(boardgame.ToList());
         }
-
-        // GET: Boardgames/Details/5
+        
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -38,25 +37,28 @@ namespace BoardgameStore.Controllers
             }
             return View(boardgame);
         }
-
-        // GET: Boardgames/Create
+        
         public ActionResult Create()
         {
-            ViewBag.author_id = new SelectList(db.Author, "author_id", "author_name");
-            ViewBag.complexity_id = new SelectList(db.Complexity, "complexity_id", "complexity_name");
-            ViewBag.publisher_id = new SelectList(db.Publisher, "publisher_id", "publisher_name");
+            List<SelectListItem> items = new SelectList(db.Author, "author_id", "author_name").ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, Selected=true}));
+            ViewBag.author_id = items;
+            items = new SelectList(db.Complexity, "complexity_id", "complexity_name").ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, Selected=true}));
+            ViewBag.complexity_id = items;
+            items = new SelectList(db.Publisher, "publisher_id", "publisher_name").ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, Selected=true}));
+            ViewBag.publisher_id = items;
             return View();
         }
-
-        // POST: Boardgames/Create
-        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
-        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "boardgame_name,min_time,max_time,min_players,max_players,years,language,boardgame_number,boardgame_cost,independence,images_paths,preview_description,full_description,boardgame_equipment,boardgame_id,author_id,publisher_id,complexity_id")] Boardgame boardgame, HttpPostedFileBase image = null)
         {
             bool checkCoverLength = true;
             bool checkCoverType = true;
+            List<SelectListItem> items;
             if (ModelState.IsValid)
             {
                 if (image != null)
@@ -70,8 +72,6 @@ namespace BoardgameStore.Controllers
                             string covername = "";
                             do
                             {
-                                //ContentType contentType = new ContentType(image.ContentType);
-                                //type = image.ContentType.Split(new char[] { '/' })[image.ContentType.Split(new char[] { '/' }).Length-1];
                                 type = System.IO.Path.GetFileName(image.FileName).Split(new char[] { '.' })[System.IO.Path.GetFileName(image.FileName).Split(new char[] { '.' }).Length - 1];
                                 covername = Path.GetRandomFileName() + "." + type;
                                 if (System.IO.File.Exists("~/Content/" + covername)) flag = true;
@@ -94,31 +94,45 @@ namespace BoardgameStore.Controllers
                 if (!checkCoverType)
                 {
                     ViewBag.CoverType = true;
-                    ViewBag.author_id = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id);
-                    ViewBag.complexity_id = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id);
-                    ViewBag.publisher_id = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id);
-
+                    items = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.author_id = items;
+                    items = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.complexity_id = items;
+                    items = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.publisher_id = items;
                     return View("Edit", boardgame);
                 }
                 else if (!checkCoverLength)
                 {
                     ViewBag.CoverLength = true;
-                    ViewBag.author_id = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id);
-                    ViewBag.complexity_id = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id);
-                    ViewBag.publisher_id = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id);
-
+                    items = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.author_id = items;
+                    items = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.complexity_id = items;
+                    items = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.publisher_id = items;
                     return View("Edit", boardgame);
                 }
                 return RedirectToAction("Catalog");
             }
 
-            ViewBag.author_id = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id);
-            ViewBag.complexity_id = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id);
-            ViewBag.publisher_id = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id);
-            return View(boardgame);
+            items = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id).ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+            ViewBag.author_id = items;
+            items = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id).ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+            ViewBag.complexity_id = items;
+            items = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id).ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+            ViewBag.publisher_id = items; return View(boardgame);
         }
-
-        // GET: Boardgames/Edit/5
+        
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -130,21 +144,25 @@ namespace BoardgameStore.Controllers
             {
                 return HttpNotFound();
             }
-            ViewBag.author_id = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id);
-            ViewBag.complexity_id = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id);
-            ViewBag.publisher_id = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id);
+            List<SelectListItem> items = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id).ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null,  }));
+            ViewBag.author_id = items;
+            items = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id).ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+            ViewBag.complexity_id = items;
+            items = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id).ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+            ViewBag.publisher_id = items;
             return View(boardgame);
         }
-
-        // POST: Boardgames/Edit/5
-        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
-        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "boardgame_name,min_time,max_time,min_players,max_players,cover_path,years,language,boardgame_number,boardgame_cost,independence,images_paths,preview_description,full_description,boardgame_equipment,boardgame_id,author_id,publisher_id,complexity_id")] Boardgame boardgame, HttpPostedFileBase image = null)
         {
             bool checkCoverLength = true;
             bool checkCoverType = true;
+            List<SelectListItem> items;
             if (ModelState.IsValid)
             {
                 if (image != null)
@@ -158,8 +176,6 @@ namespace BoardgameStore.Controllers
                             string covername = "";
                             do
                             {
-                                //ContentType contentType = new ContentType(image.ContentType);
-                                //type = image.ContentType.Split(new char[] { '/' })[image.ContentType.Split(new char[] { '/' }).Length-1];
                                 type = System.IO.Path.GetFileName(image.FileName).Split(new char[] { '.' })[System.IO.Path.GetFileName(image.FileName).Split(new char[] { '.' }).Length - 1];
                                 covername = Path.GetRandomFileName() + "." + type;
                                 if (System.IO.File.Exists("~/Content/" + covername)) flag = true;
@@ -182,31 +198,48 @@ namespace BoardgameStore.Controllers
                 if (!checkCoverType)
                 {
                     ViewBag.CoverType = true;
-                    ViewBag.author_id = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id);
-                    ViewBag.complexity_id = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id);
-                    ViewBag.publisher_id = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id);
 
+                    items = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.author_id = items;
+                    items = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.complexity_id = items;
+                    items = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.publisher_id = items;
                     return View(boardgame);
                 }
                 else if (!checkCoverLength)
                 {
                     ViewBag.CoverLength = true;
-                    ViewBag.author_id = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id);
-                    ViewBag.complexity_id = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id);
-                    ViewBag.publisher_id = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id);
 
+                    items = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.author_id = items;
+                    items = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.complexity_id = items;
+                    items = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id).ToList();
+                    items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+                    ViewBag.publisher_id = items;
                     return View(boardgame);
                 }
                 return RedirectToAction("Catalog", db.Boardgame);
             }
-            ViewBag.author_id = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id);
-            ViewBag.complexity_id = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id);
-            ViewBag.publisher_id = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id);
-            
+
+            items = new SelectList(db.Author, "author_id", "author_name", boardgame.author_id).ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+            ViewBag.author_id = items;
+            items = new SelectList(db.Complexity, "complexity_id", "complexity_name", boardgame.complexity_id).ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+            ViewBag.complexity_id = items;
+            items = new SelectList(db.Publisher, "publisher_id", "publisher_name", boardgame.publisher_id).ToList();
+            items.Insert(0, (new SelectListItem { Text = null, Value = null, }));
+            ViewBag.publisher_id = items;
             return View(boardgame);
         }
 
-        // GET: Boardgames/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -220,8 +253,7 @@ namespace BoardgameStore.Controllers
             }
             return View(boardgame);
         }
-
-        // POST: Boardgames/Delete/5
+        
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -233,7 +265,7 @@ namespace BoardgameStore.Controllers
             }
             db.Boardgame.Remove(boardgame);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("Catalog", db.Boardgame);
         }
 
         protected override void Dispose(bool disposing)
@@ -311,11 +343,13 @@ namespace BoardgameStore.Controllers
                 });
             }
             db.SaveChanges();
+            ViewBag.Sum = db.User.Find(1).Basket.Sum(b => b.product_number * b.product_number);
             return View("Basket", db.User.Find(1).Basket);
         }
 
         public ActionResult Basket()
         {
+            ViewBag.Sum = db.User.Find(1).Basket.Sum(b => b.Boardgame.boardgame_cost*b.product_number);
             return View(db.User.Find(1).Basket);
         }
 
@@ -336,7 +370,7 @@ namespace BoardgameStore.Controllers
                 db.Basket.Remove(basket);
             }
             db.SaveChanges();
-            return View("Basket", db.User.Find(1).Basket);
+            return RedirectToAction("Basket", db.User.Find(1).Basket);
         }
 
         public ActionResult IncreaseOrder(int? id)
@@ -356,7 +390,7 @@ namespace BoardgameStore.Controllers
                 db.User.Find(1).Basket.Remove(basket);
             }
             db.SaveChanges();
-            return View("Basket", db.User.Find(1).Basket);
+            return RedirectToAction("Basket", db.User.Find(1).Basket);
         }
 
         public ActionResult DeleteOrder(int? id)
@@ -372,7 +406,7 @@ namespace BoardgameStore.Controllers
             }
             db.Basket.Remove(basket);
             db.SaveChanges();
-            return View("Basket", db.User.Find(1).Basket);
+            return RedirectToAction("Basket", db.User.Find(1).Basket);
         }
     }
 }
